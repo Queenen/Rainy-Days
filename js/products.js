7704cf872b9b4ee0f93d569ffe34c2ead8973170
Queenen <AnnBjo66962@stud.noroff.no>
2023-04-20 21:13:47 +0200

    small changes

--- js/products.js
+++ js/products.js
@@ -1,6 +1,17 @@
 const productContainer = document.querySelector("#products_container");
 
-const url = "http://rainy-days.local/wp-json/wc/store/products";
+let url = "http://rainy-days.local/wp-json/wc/store/products";
+console.log(url);
+
+// Featured button
+
+const featuredBtn = document.querySelector(".featured_btn");
+
+// Change url on click = changes results showing
+featuredBtn.addEventListener("click", () => {
+  url = "http://rainy-days.local/wp-json/wc/store/products?featured=true";
+  console.log(url);
+});
 
 async function fetchProducts() {
   try {
