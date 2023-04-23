f0ada3b996ac17c0621f6d1aaf1f981a40cb1f41
Queenen <AnnBjo66962@stud.noroff.no>
2023-04-23 15:19:25 +0200

    contact

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,14 @@
 const sendMessage = document.querySelector(".send_message");
 const form = document.querySelector("#contact_form");
 
-sendMessage.addEventListener("click", () => {
-  form.innerHTML = `
-    <div class="contact_message_sent">
-    <p class="title bold">Your message got sent!</p>
-    <p>We will get back to you as soon as possible!</p>
-    </div>`;
-});
+// Form validation
+
+const fullName = document.querySelector("#full_name");
+const email = document.querySelector("#email");
+const subject = document.querySelector("#subject");
+const message = document.querySelector("#message");
+//error messages
+const nameError = document.querySelector(".name_error");
+const emailError = document.querySelector(".email_error");
+const subjectError = document.querySelector(".subject_error");
+const messageError = document.querySelector(".message_error");
